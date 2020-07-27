01e0309980ae06f02bfd6557dfb09d89e2a4ae8b
brian <riceb53@gmail.com>
2020-07-27 18:44:32 -0400

    initialcommit

---
+++ js_examples.js
@@ -0,0 +1,36 @@
+function printNumbersDivisibleByThree() {
+  var index = 1;
+  while (index <= 1000) {
+    if (index % 3 === 0) {
+      console.log(index);
+    }
+    index += 1;
+  }
+}
+printNumbersDivisibleByThree();
+
+function printEveryOtherItem(strings) {
+  var index = 0;
+  strings.forEach(function(string) {
+    if (index % 2 === 0) {
+      console.log(string);
+    }
+    index += 1;
+  });
+}
+printEveryOtherItem(["a", "b", "c", "d", "e"]);
+
+function computeSum(numbers) {
+  var sum = 0;
+  numbers.forEach(function(number) {
+    sum += number;
+  });
+  return sum;
+}
+console.log(computeSum([2, 4, 5]));
+
+
+var cityPopulations = {chi: 2700000};
+cityPopulations["nyc"] = 8400000;
+cityPopulations.sf = 800000;  // same as cityPopulations["sf"]
+console.log(cityPopulations);
